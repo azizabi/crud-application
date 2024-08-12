ac593e3ed426c673b3eeec0e0f3db99a4c835d3e
Abinesh <freakaziz07@gmail.com>
2024-08-12 23:49:47 +0530

    route-structure

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ app.get('/',(req,res)=>{
     res.json({msg :"hello"})
 })
 
+// CRUD functions
+// r -
+app.get('/movies',(req,res)=>{})
+
+//c -
+app.post('/movies',(req,res)=>{})
+
+// u
+app.put('/movies/:id',(req,res)=>{})
+
+//d
+app.delete('/movies/:id',(req,res)=>{})
+
 app.listen(port,()=>{
     console.log(`the server is running http://localhost:${port}`);
     
